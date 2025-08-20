6ded3275e69757c146f88bcc4aa3006d9e0647b9
Wyatt Joseph Cassiotis <wyatt.cassiotis@gmail.com>
2025-08-20 02:36:48 -0400

    Done adding most extensions

--- doc/src/app/documents/[documentId]/editor.tsx
+++ doc/src/app/documents/[documentId]/editor.tsx
@@ -7,6 +7,8 @@ import Table from '@tiptap/extension-table';
 import TableCell  from '@tiptap/extension-table-cell';
 import TableHeader from '@tiptap/extension-table-header';
 import  TableRow  from '@tiptap/extension-table-row';
+import Image from '@tiptap/extension-image'
+import ImageResize from 'tiptap-extension-resize-image'
 export const Editor = () => {
     const editor = useEditor({
         editorProps: {
@@ -22,12 +24,28 @@ export const Editor = () => {
             TableCell,
             TableRow,
             TableHeader,
-
+            Image,
+            ImageResize,
             TaskItem.configure({
                 nested: true,
             }),
         ],
-        content: '<p>Hello World!<p/>',
+            content: `
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th colspan="3">Description</th>
+            </tr>
+            <tr>
+              <td>Cyndi Lauper</td>
+              <td>Singer</td>
+              <td>Songwriter</td>
+              <td>Actress</td>
+            </tr>
+          </tbody>
+        </table>
+      `,
     })
     return(
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px=4 print:p-0 print:bg-white print:overflow-visible'>
