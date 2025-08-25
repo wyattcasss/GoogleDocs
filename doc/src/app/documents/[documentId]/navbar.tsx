e714c7bf247b9ad3793a5a2048ff1cbb54fc4fa4
Wyatt Joseph Cassiotis <wyatt.cassiotis@gmail.com>
2025-08-25 02:17:29 -0400

    Basic nav bar

---
+++ doc/src/app/documents/[documentId]/navbar.tsx
@@ -0,0 +1,63 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import{
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarSub,
+    MenubarSubContent,
+    MenubarSubTrigger,
+    MenubarTrigger
+} from "@/components/ui/menubar"
+
+import { DocumentInput } from "./document-input"
+import { FileIcon } from "lucide-react"
+export const Navbar = () => {
+    return(
+        <nav className=" flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+                <Link href="/">
+                    <Image src="/logo.svg" alt="Logo" width={43} height={43}/>
+                </Link>
+                <div className="flex flex-col">
+                    <DocumentInput/>
+                    <div className="flex">
+                        <Menubar className="border-none bg-transparent shadow-none h-auto p-0">
+                            <MenubarMenu>
+                                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                                    File
+                                </MenubarTrigger>
+                                <MenubarContent>
+                                    <MenubarItem>
+                                        <FileIcon className="size-4 mr-2"/>
+                                        Save
+                                    </MenubarItem>
+                                </MenubarContent>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                                    Edit
+                                </MenubarTrigger>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                                    Insert
+                                </MenubarTrigger>
+                            </MenubarMenu>
+                            <MenubarMenu>
+                                <MenubarTrigger className="text-sm font-normal py-0.5 px-[7px] rounded-sm hover:bg-muted h-auto">
+                                    Format
+                                </MenubarTrigger>
+                            </MenubarMenu>
+                        </Menubar>
+                    </div>
+                </div>  
+            </div>
+                
+        </nav>
+    )
+}
